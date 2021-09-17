282aea1e93a8a78b2774ac8c125d3e4d21aa189f
AbhiGene <abhinav.godavarthi@yale.edu>
2021-09-17 11:21:04 -0400

    Fixed naming error

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 if sys.version_info[:2] < (3, 6):
     raise RuntimeError("Python version >=3.6 required.")
 
-version_py = os.path.join(os.path.dirname(__file__), "CATCH", "version.py")
+version_py = os.path.join(os.path.dirname(__file__), "catch-test", "version.py")
 version = open(version_py).read().strip().split("=")[-1].replace('"', "").strip()
 
 readme = open("README.md").read()
